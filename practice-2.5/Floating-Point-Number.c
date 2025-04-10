52b80a5c6b10901a2b7e2268b6bc26131f4b573f
hrmrakib <hrmrakibs@gmail.com>
2025-04-10 11:52:13 +0600

    Floating Point Number

---
+++ practice-2.5/Floating-Point-Number.c
@@ -0,0 +1,37 @@
+/*
+Floating Point Number
+Time Limit: 1 Seconds
+
+Memory Limit: 2.93 MB
+
+Statement
+In this problem you will be given a floating point number x. You have to print x upto 3 decimal points.
+
+Constraints
+1.0 <= x <= 100.0
+
+Input format
+Input will contain a floating point number x.
+
+Output Format
+Print the number x upto 3 decimal points.
+
+Sample Input 1
+22.12344
+
+Sample Output 1
+22.123
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    float n;
+
+    scanf("%f", &n);
+
+    printf("%.3f", n);
+ 
+    return 0; 
+}
