f19f0e7e9f3b13c6b744ec732869678d6a46687a
hrmrakib <hrmrakibs@gmail.com>
2025-04-14 10:29:08 +0600

    Even Odd Positive and Negative

---
+++ test/testCode.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    int x = 5;
+    int y = 4;
+
+    if(x >= y && x < y + 4) {
+        if(y < 5) {
+            printf("hi ");
+        }if(y < 10){
+    printf("%d ------ ", y);
+
+            printf("hi ");
+        }
+    printf("%d", y);
+
+    }else {
+        printf("hello");
+    }
+    printf("%d", y);
+    return 0;
+}
