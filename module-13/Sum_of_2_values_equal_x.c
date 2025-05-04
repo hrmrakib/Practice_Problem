ba8fccd4951828cdd1a0b412562f78607b3b0560
hrmrakib <hrmrakibs@gmail.com>
2025-05-04 19:54:59 +0600

    Refactor logic to improve clarity and efficiency in finding two values that sum to x

--- module-13/Sum_of_2_values_equal_x.c
+++ module-13/Sum_of_2_values_equal_x.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<stdbool.h>
 
 int main() {
  
@@ -13,13 +14,20 @@ int main() {
     
     int x;
     scanf("%d", &x);
+ 
+    bool flag = true;
 
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
             if(a[i] + a[j] == x) {
                 printf("%d (%d - index) + %d (%d - index) = %d", a[i], i, a[j], j, a[i] + a[j]);
+                flag = false;
             }
         }
+    }
+
+    if(flag) {
+        printf("Not possible!");
     }
 
     return 0;
