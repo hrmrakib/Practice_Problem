80efbaa548e396e2f712b45db575f950a5ecef92
hrmrakib <hrmrakibs@gmail.com>
2025-05-09 22:13:29 +0600

    solve phlindrome

--- assignment-03/Is_Palindrome.c
+++ assignment-03/Is_Palindrome.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-is_palindrome()
+int is_palindrome(char s[], int len)
 {
-}
-
-int main()
-{
-
-    char s[1001];
-    scanf("%s", &s);
-
     char x[1001];
-
-    int len = strlen(s);
 
     for (int i = len; i > 0; i--)
     {
         x[len - i] = s[i - 1];
     }
 
-    // int len = strlen(s);
-    // for (int i = len; i > 0; i++)
-    // {
-        
-    // }
+    if (strcmp(s, x) == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
-    printf("%s", x);
+int main()
+{
+
+    char s[1001];
+
+    scanf("%s", s);
+
+    int len = strlen(s);
+
+    int result = is_palindrome(s, len);
+
+    if(result == 1)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not Palindrome");
+    }
 
     return 0;
 }
