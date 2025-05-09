844bd73baee513ae0c1bf64d534688369ea7a105
hrmrakib <hrmrakibs@gmail.com>
2025-05-09 14:41:42 +0600

    Pailndrome

---
+++ assignment-03/Is_Palindrome.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+// is_palindrome()
+// {
+// }
+
+int main()
+{
+
+    char s[1001];
+    scanf("%s", &s);
+
+    char x[1001];
+
+    int len = strlen(s);
+    for (int i = len; i > 0; i++)
+    {
+        
+    }
+
+    printf("%s", x);
+
+    return 0;
+}
