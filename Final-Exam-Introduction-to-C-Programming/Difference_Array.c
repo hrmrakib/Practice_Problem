7707d74773625bb48030588ff9ef37980f8e3a98
hrmrakib <hrmrakibs@gmail.com>
2025-05-18 11:33:43 +0600

    Solved: Difference Array

--- Final-Exam-Introduction-to-C-Programming/Difference_Array.c
+++ Final-Exam-Introduction-to-C-Programming/Difference_Array.c
@@ -1,34 +1,54 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
+int main()
+{
+    int tc;
+    scanf("%d", &tc);
     
-    int n;
-    scanf("%d", &n);
+    for (int x = 0; x < tc; x++)
+    {
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
 
-    int a[n];
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-    }
+        int b[n];
+        int c[n];
 
-    int b[n]; 
+        // copy a arr & set to the b arr
+        for (int i = 0; i < n; i++)
+        {
+            b[i] = a[i];
+        }
 
-    for(int i = 0; i < n; i++) {
-        b[i] = a[i];
-    }
-
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(b[i] < b[j]) {
-                int temp = b[i];
-                b[i] = b[j];
-                b[j] = temp;
+        // ascending order of b arr
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (b[i] < b[j])
+                {
+                    int temp = b[i];
+                    b[i] = b[j];
+                    b[j] = temp;
+                }
             }
         }
+
+        for (int i = 0; i < n; i++)
+        {
+            int cal = a[i] - b[i];
+            c[i] = cal;
+        }
+
+        for(int i = 0; i < n; i++){
+            printf("%d ", abs(c[i]));
+        }
+        printf("\n");
     }
-    
-    for(int i = 0; i < n; i++) {
-        printf("%d", b[i]);
-    }
-    
     return 0;
 }
