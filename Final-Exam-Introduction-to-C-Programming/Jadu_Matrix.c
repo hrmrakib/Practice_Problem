9b11775f49b966cb83f80d0fe39ff2c31510f4c2
hrmrakib <hrmrakibs@gmail.com>
2025-05-18 06:56:23 +0600

    Solved: Jadu Matrix

--- Final-Exam-Introduction-to-C-Programming/Jadu_Matrix.c
+++ Final-Exam-Introduction-to-C-Programming/Jadu_Matrix.c
@@ -1,53 +1,60 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
-    
-    int n , m;
+int main()
+{
+
+    int n, m;
     scanf("%d %d", &n, &m);
 
     int a[n][m];
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             scanf("%d", &a[i][j]);
         }
     }
 
     bool isJaduMatrix = true;
 
-    int count = 0;
+    if (n == m)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if (i != j && i + j != n - 1) {
-                count += 1;
-                printf("Zero => %d\n", i != j && i + j != n - 1);
+                if (i == j || (i + j) == n - 1)
+                {
+                    if (a[i][j] != 1)
+                    {
+                        isJaduMatrix = false;
+                    }
+                }
+                else
+                {
+                    if (a[i][j] != 0)
+                    {
+                        isJaduMatrix = false;
+                    }
+                }
             }
-
-            // if(i == j) { // for primary diagonal
-            //     if(a[i][j] == 1) {
-            //         isJaduMatrix = true;
-            //         printf("YES\n");
-            //     }else{
-            //         isJaduMatrix = false;
-            //     }
-            // }
-
-            // if(i + j == n-1) { // for secondary diagonal 
-            //     if(a[i][j] == 1){
-            //         printf("==> %d", a[i][j]);
-            //         isJaduMatrix = true;
-            //     }else{
-            //         isJaduMatrix = false;
-            //     }
-            // }
         }
-        printf("\n");
+        if (isJaduMatrix == true)
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
     }
-
-    // printf("\n");
-    printf("Answer: %d", isJaduMatrix);
+    else
+    {
+        printf("NO");
+    }
 
     return 0;
 }
